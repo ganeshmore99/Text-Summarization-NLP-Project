1a9a11568f9555713f1081e01c94c607449d1c93
ganeshmore99 <ganeshmore5199@gmail.com>
2023-09-13 10:31:15 +0530

    Model Evaluation

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -3,7 +3,8 @@ from textSummarizer.utils.common import read_yaml, create_directories
 from textSummarizer.entity import (DataIngestionConfig,
                                    DataValidationConfig,
                                    DataTransformationConfig,
-                                   ModelTrainerConfig)
+                                   ModelTrainerConfig,
+                                   ModelEvaluationConfig)
 
 
 class ConfigurationManager:
@@ -11,13 +12,19 @@ class ConfigurationManager:
         self,
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
+
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
+
         create_directories([self.config.artifacts_root])
+
     
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
+
         create_directories([config.root_dir])
+
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
             source_URL=config.source_URL,
@@ -26,7 +33,7 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-
+    
 
 
     def get_data_validation_config(self) -> DataValidationConfig:
@@ -41,6 +48,7 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
 
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
@@ -54,8 +62,9 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
-
     
+
+
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.TrainingArguments
@@ -78,3 +87,20 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+           
+        )
+
+        return model_evaluation_config
